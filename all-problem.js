ee0c24084aaf47dd1c4bce366105103d86666f49
israt <isratjahanpanna87@gmail.com>
2023-08-09 21:36:15 +0600

    problem 5

--- all-problem.js
+++ all-problem.js
@@ -69,3 +69,21 @@ function findAddress(obj) {
   
 
 } 
+function canPay(changeArray, totalDue) {
+  if (changeArray.length != 0) {
+    let sum = 0;
+    for (let i = 0; i < changeArray.length; i++) {
+      const index = i;
+      const element = changeArray[index];
+      sum = sum + element;
+    }
+    if (sum >= 10) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return "Your array input is empty please enter a valid array input";
+  }
+}
+
--- problem-5.js
+++ problem-5.js
@@ -1,2 +1,21 @@
-function canPay(changeArray, totalDue) { 
+function canPay(changeArray, totalDue) {
+  if (changeArray.length != 0) {
+    let sum = 0;
+    for (let i = 0; i < changeArray.length; i++) {
+      const index = i;
+      const element = changeArray[index];
+      sum = sum + element;
+    }
+    if (sum >= 10) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return "Your array input is empty please enter a valid array input";
+  }
 }
+const inputArray = [];
+const requireBalance = 10;
+const output = canPay(inputArray, requireBalance);
+console.log(output);
